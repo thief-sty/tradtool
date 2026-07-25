0f436e3ae921084ff9c95fab9b14474fbea4cfa6
thief-sty <11363816+thief-sty@users.noreply.github.com>
2026-07-25 21:48:09 +0200

    feat: add code editor components

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,13 @@
+use kode_leptos::CodeEditor;
 use leptos::prelude::*;
 
 fn main() {
-    leptos::mount::mount_to_body(|| view! {})
+    leptos::mount::mount_to_body(|| {
+        view! {
+            <main style="display: flex; height: 100vh;">
+                <CodeEditor />
+                <CodeEditor />
+            </main>
+        }
+    })
 }
